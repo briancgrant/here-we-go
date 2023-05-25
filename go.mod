187f0a9b9eff7ec484aa506d0f84db89ea7c5f8c
Brian Grant <57539893+briancgrant@users.noreply.github.com>
2023-05-25 13:48:54 +0000

    follow along result

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jnguyen0220/here-we-go
 
 go 1.20
+
+require github.com/jnguyen0220/here-we-go-function v0.0.0-20230525030556-76340727f409 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+
+	jn "github.com/jnguyen0220/here-we-go-function/intro"
+)
+
 func main() {
 	// 1. Hello World
+	fmt.Println("Hello World")
+	// 2. Custom Function - Use Command: 'go run .' to include package files
+	test("There")
+	// 3. Third Party Library
+	fmt.Println(rand.Intn(100))
+	// 4. GitHub Package (1st Party Library)
+	jn.Hello()
+	// 5. Http Server
+	port := 9090
+	http.HandleFunc("/", handler)
+	fmt.Printf("Http server running on :%v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
 
-	// 2. Custome Function
+func handler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "Hi\n")
 }
